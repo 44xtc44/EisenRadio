0aac12c233f33d6f157eac8c1a43031db3342676
4xtc <44xtc44@gmail.com>
2022-11-01 15:34:51 +0100

    add EisenApi class to implement skipped rec display

--- eisenradio/api/__init__.py
+++ eisenradio/api/__init__.py
@@ -1,3 +1,4 @@
+from ghettorecorder import ghettoApi
 
 
 class Api:
@@ -39,5 +40,54 @@ class GhettoTest:
         self.thread_killer = thread_killer
 
 
+class EisenApi:
+    """ attributes and methods only needed for eisenradio
+    todo:
+        refactor GhettoApi of GhettoRecorder package to transfer all those attributes to here
+    """
+    def __init__(self):
+        """ init
+        Attributes:
+                radio_id_dict, {radio1: 1, radio2: 2} radio name to db table id mapping (from {id: name})
+                skipped_record_eisen_dict, {radio1: 2} compare list len of Ghetto skipped_in_session_dict with counter
+                call_skipped_record_eisen_dict, endpoint call before get_skipped_record()
+        """
+        self.radio_id_dict = {}
+        self.call_radio_id_dict = True
+        self.skipped_record_eisen_dict = {}
+        self.call_skipped_record_eisen_dict = True
+
+    def init_radio_id_dict(self):
+        if self.call_radio_id_dict:
+            self.radio_id_dict = {name: table_id for table_id, name in ghettoApi.radios_in_view_dict.items()}
+            self.call_radio_id_dict = False
+
+    def init_skipped_record_eisen_dict(self):
+        """ get radio key name from GhettoApi all_blacklists_dict {radio1: [], radio2: []} ([] is title skip list)
+        set key value to zero to see if GhettoApi skipped_in_session_dict was updated later
+        only called once
+        """
+        if self.call_skipped_record_eisen_dict:
+            self.skipped_record_eisen_dict = {radio: 0 for radio in ghettoApi.all_blacklists_dict.keys()}
+            self.call_skipped_record_eisen_dict = False
+
+    def get_skipped_record(self, radio_name):
+        """ return True if a title was skipped since last request, False if not
+        check EisenApi skipped_record_eisen_dict radio:count lower than in length of GhettoApi skipped_in_session_dict
+        update skipped_record_eisen_dict[radio_name] to new length of ghettoApi.skipped_in_session_dict[radio_name] list
+        """
+        if not len(ghettoApi.skipped_in_session_dict):
+            return False
+        if radio_name not in ghettoApi.skipped_in_session_dict.keys():
+            return False
+
+        if self.skipped_record_eisen_dict[radio_name] < len(ghettoApi.skipped_in_session_dict[radio_name]):
+            self.skipped_record_eisen_dict[radio_name] = len(ghettoApi.skipped_in_session_dict[radio_name])
+            return True
+        else:
+            return False
+
+
 api = Api()
 ghettoTest = GhettoTest()
+eisenApi = EisenApi()
