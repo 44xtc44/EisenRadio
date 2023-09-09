c7b3ae0d05a244ec34194d0b63e11b2817090339
4xtc <44xtc44@gmail.com>
2023-09-09 18:17:22 +0200

    rm whitespace

--- eisenradio/app.py
+++ eisenradio/app.py
@@ -25,8 +25,7 @@ print('\n Python WSGI "Waitress" serves flask\n')
 
 
 def is_port_occupied(srv_port: int) -> bool:
-    """The error indicator is 0 if the operation succeeded,
-    otherwise return the value of the errno variable. 
+    """The error indicator is 0 if the operation succeeded, otherwise return the value of the errno variable.
     """
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         return s.connect_ex(('localhost', srv_port)) == 0
