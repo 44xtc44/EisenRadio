6183d17d08db7a9707e26eb6e8e7c9f2efcbd738
4xtc <44xtc44@gmail.com>
2023-09-09 09:50:27 +0200

    check port num is vacant

--- eisenradio/app.py
+++ eisenradio/app.py
@@ -1,6 +1,7 @@
 """ production server with random port assignment """
 import sys
-import random
+import socket
+# import random
 from os import path
 from waitress import serve
 from eisenradio import wsgi
@@ -18,14 +19,29 @@ sys.path.append(path.abspath(utils_dir))
 sys.path.append(path.abspath(instance_dir))
 sys.path.append(path.abspath(lib_dir))
 
-port = random.randint(12488, 12974)    # random between two integers
-app = wsgi.wsgi_app(port)
-
+# port = random.randint(12488, 12974)    # random between two integers
+port = 5050
 print('\n Python WSGI "Waitress" serves flask\n')
 
 
+def is_port_in_use(srv_port: int) -> bool:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex(('localhost', srv_port)) == 0
+
+
 def main():
-    serve(app, host='localhost', port=port)
+    global port
+
+    while 1:
+        port_vacant = is_port_in_use(port)
+        if port_vacant:
+            app = wsgi.wsgi_app(port)  # transfer port num info to flask (for audio endpoint ...:5050/sound/starFM)
+            serve(app, host='localhost', port=port)
+            break
+        else:
+            print(f'serverPort in use {port}')
+            print('\n\tadd one to port number')
+            port += 1
 
 
 if __name__ == "__main__":
